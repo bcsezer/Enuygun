03ac0aa6cdc8b198095aaca13beeafa3bcb79249
cemsezeroglu <cemsezeroglu@hotmail.com>
2024-02-25 23:36:58 +0300

    favorites basket and detail page added

---
+++ Enuygun/Constants/Color.swift
@@ -0,0 +1,8 @@
+//
+//  Color.swift
+//  Enuygun
+//
+//  Created by cem sezeroglu on 25.02.2024.
+//
+
+import Foundation
---
+++ Enuygun/Screens/ProductDetail/Cells/DetailBannerCell.swift
@@ -0,0 +1,17 @@
+//
+//  DetailBannerCell.swift
+//  Enuygun
+//
+//  Created by cem sezeroglu on 25.02.2024.
+//
+
+import UIKit
+
+class DetailBannerCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
